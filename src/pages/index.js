05a237d4081a4f8d24785cc6e31793f66ec513d2
Jemuel Balangcod <jemuelb@gmail.com>
2021-07-17 14:37:52 -0700

    Added image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import {StaticImage} from "gatsby-plugin-image"
 
 const IndexPage = () => {
     return (
-        <Layout pageTitle="Home">
-            <p>
-                Hello!
-            </p>
+        <Layout pageTitle="Home Page">
+            <StaticImage
+                alt="Coming home"
+                src="../images/coming_home.jpg"
+            />
         </Layout>
     )
 }
+
 
 export default IndexPage
