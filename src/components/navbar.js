fafb880cc894def78475b8b4860af49cd909c38b
Jemuel Balangcod <jemuelb@gmail.com>
2021-07-24 08:56:42 -0700

    Updated styling

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,7 +5,7 @@ import {css} from "@emotion/react"
 const navBranding = css`
   font-weight: bold;
   text-decoration: none;
-  color: black;
+  color: #ede7e3;
 `
 
 const navLinks = css`
@@ -19,26 +19,43 @@ const navLinkItem = css`
 `
 
 const navLinkText = css`
-  color: black;
+  color: #ede7e3;
+`
+
+const headerContainer = css`
+  background-color: #2D3142;
+  font-family: sans-serif;
+  height: 60px;
+  box-shadow: 0 0 25px 0 darkgray;
+  display: flex;
+  align-items: center;
+`
+
+const navContent = css`
+  width: 800px;
+  margin-left: auto;
+  margin-right: auto;
 `
 
 const Navbar = () => {
     return (
-        <nav>
-            <ul css={navLinks}>
-                <li css={navLinkItem}>
-                    <Link to="/" css={navBranding}>Jemuel Balangcod</Link>
-                </li>
-                <li css={navLinkItem}>
-                    <Link to="/" css={navLinkText}>Home</Link>
-                </li>
-                <li css={navLinkItem}>
-                    <Link to="/about" css={navLinkText}>About</Link>
-                </li>
-                <li css={navLinkItem}>
-                    <Link to="/blog" css={navLinkText}>Blog</Link>
-                </li>
-            </ul>
+        <nav css={headerContainer}>
+            <div css={navContent}>
+                <ul css={navLinks}>
+                    <li css={navLinkItem}>
+                        <Link to="/" css={navBranding}>Jemuel Balangcod</Link>
+                    </li>
+                    <li css={navLinkItem}>
+                        <Link to="/" css={navLinkText}>Home</Link>
+                    </li>
+                    <li css={navLinkItem}>
+                        <Link to="/about" css={navLinkText}>About</Link>
+                    </li>
+                    <li css={navLinkItem}>
+                        <Link to="/blog" css={navLinkText}>Blog</Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
